3dae6feada5c39f435dfda6623f5486526e6f89a
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-04 10:16:33 -0300

    Senha aleatória concluída

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,64 @@
 // Seleção de Elementos
 const generatePasswordButton = document.querySelector("#generate-password");
-const generatedPasswordButton = document.querySelector("#generated-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
 //Funções
-//Letras 
+//Letras
 const getLetterLowerCase = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 const getLetterUpperCase = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 //Números
 const getNumber = () => {
-    return Math.floor(Math.random() * 10).toString();
+  return Math.floor(Math.random() * 10).toString();
 };
 //Símbolos
 const getSymbol = () => {
-    const symbols = "(){}[]=<>/,.!@#$%¨&*-+`~^?;:"
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = "(){}[]=<>/,.!@#$%¨&*-+`~^?;:";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+//Unir todas as funções acima
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  //A senha  começa vazia
+  let password = "";
+
+  //Tamanho da senha
+  const passwordLength = 10;
+
+  //Lista de opções para a senha
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+  for (i = 0; i < passwordLength; i = i + 4) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+  password = password.slice(0, passwordLength);
+  
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
 };
 
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-    console.log ("teste");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  );
 });
