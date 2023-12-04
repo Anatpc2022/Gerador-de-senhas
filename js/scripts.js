c22eb8a5b9ac3836e3d999b12fd67da40ca92a91
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-04 09:33:10 -0300

    Função criada de Letras, Números e Símbolos  aleatórios

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-console.log("teste");
+// Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordButton = document.querySelector("#generated-password");
+
+//Funções
+//Letras 
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+//Números
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+//Símbolos
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%¨&*-+`~^?;:"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log ("teste");
+});
